50674f100b097c4b056094e4d4d0fb225db9e765
tera-saki <tera_saki@icloud.com>
2022-09-21 00:55:09 +0900

    feat: change sed option depending on the type of OS
    squash! feat: change sed option depending on the type of OS

--- setup.sh
+++ setup.sh
@@ -18,9 +18,14 @@ MYSQL_DATABASE="atcoder_problems"
 mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8mb4;"
 
 # replace dummy config
-# if you use Mac and error occurs, try installing gsed and replacing sed with gsed in the following lines.
-sed -i "s/__REPLACE_TO_MYSQL_USER__/${MYSQL_USER}/g" config/database.json
-sed -i "s/__REPLACE_TO_MYSQL_PASSWORD__/${MYSQL_PASSWORD}/g" config/database.json
+if [ "$(uname)" == "Darwin" ]
+then
+    SED_COMMAND='sed -i .bak'
+else
+    SED_COMMAND='sed -i'
+fi
+${SED_COMMAND} "s/__REPLACE_TO_MYSQL_USER__/${MYSQL_USER}/g" config/database.json
+${SED_COMMAND} "s/__REPLACE_TO_MYSQL_PASSWORD__/${MYSQL_PASSWORD}/g" config/database.json
 
 # install npm packages
 yarn
